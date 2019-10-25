0075ec372bdf7d9d20954a5b6bc5ed53f6ec4d67
Essa Hijazi <essahijazi18@gmail.com>
2019-10-25 10:08:10 +0200

    variable naming rules and conventions

---
+++ Main.java
@@ -0,0 +1,29 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		
+		/*
+		 * Acceptable variable names
+		 * can use numbers, letters, $ and _
+		 * use camelCase and don't start variable
+		 *  with a number or special character
+		*/
+		int total;
+		int grade4;
+		int bankAccountBalance;
+		
+		
+		/*
+ 		 * Unacceptable variable names:
+ 		 * 
+ 		 * String phone#
+ 		 * String address&State
+ 		 * 
+		 */
+	}
+
+}
+ 
